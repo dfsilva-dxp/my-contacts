a9b8cd0ae4a105cd43161b2023123be7a0d238d2
Daniel Silva <dfsilva.dxp@gmail.com>
2023-08-26 13:14:36 -0300

    style: refactored button style.

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -10,11 +10,8 @@ export const ButtonContent = styled.button<ButtonContentProps>`
     border: 2px solid transparent;
     border-radius: 0.25rem;
     flex-shrink: 0;
-
-    &:hover,
-    &:active {
-      filter: brightness(0.9);
-    }
+    font-weight: ${theme.font.weight.bold};
+    transition: all 0.15s ease-in-out;
 
     ${!!variant && buttonVariants[variant](theme)}
     ${!!size && buttonVariants[size]}
@@ -23,18 +20,41 @@ export const ButtonContent = styled.button<ButtonContentProps>`
 
 const buttonVariants = {
   primary: (theme: DefaultTheme) => css`
-    background-color: ${theme.colors.primaryMain};
-    color: ${theme.colors.whiteMain};
+    background-color: ${theme.colors.primary.main};
+    color: ${theme.colors.white};
+
+    &:hover,
+    &:active {
+      background-color: ${theme.colors.primary.dark};
+    }
   `,
   neutral: (theme: DefaultTheme) => css`
-    background-color: ${theme.colors.whiteMain};
+    background-color: transparent;
+
+    &:hover,
+    &:active {
+      background-color: ${theme.colors.white};
+    }
   `,
   ghost: (theme: DefaultTheme) => css`
-    background-color: ${theme.colors.whiteMain};
+    background-color: transparent;
+    border-color: ${theme.colors.primary.main};
+    color: ${theme.colors.primary.main};
+
+    &:hover,
+    &:active {
+      background-color: ${theme.colors.primary.main};
+      color: ${theme.colors.white};
+    }
   `,
   danger: (theme: DefaultTheme) => css`
-    background-color: ${theme.colors.dangerMain};
-    color: ${theme.colors.whiteMain};
+    background-color: ${theme.colors.danger.main};
+    color: ${theme.colors.white};
+
+    &:hover,
+    &:active {
+      background-color: ${theme.colors.danger.dark};
+    }
   `,
   small: () => css`
     font-size: 0.875rem;
