0582d9ede05b4360b23c008f456b225207d6b657
Daniel Silva <dfsilva.dxp@gmail.com>
2023-09-20 12:05:32 -0300

    refactor: refactor select component with radix-ui.

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,36 +1,61 @@
-import * as S from "./styles";
+import * as Select from "@radix-ui/react-select";
+import { CaretDown, CaretUp, Check } from "phosphor-react";
 
-export interface ISelectContentProps {
-  borderRadius?: "default" | "full";
-  hasBoxShadow?: boolean;
-}
+import * as S from "./styles";
 
 type SelectProps = {
   placeholder?: string;
+  selectLabel?: string;
   options: {
     label: string;
     value: string;
   }[];
-} & ISelectContentProps;
+};
 
-const Select = ({
+const SelectInput = ({
   placeholder = "Selecione uma categoria",
-  options,
-  borderRadius = "default",
-  hasBoxShadow = true
+  selectLabel,
+  options
 }: SelectProps) => {
   return (
-    <S.SelectContent borderRadius={borderRadius} hasBoxShadow={hasBoxShadow}>
-      <S.SelectInput>
-        <S.SelectPlaceholder>{placeholder}</S.SelectPlaceholder>
-        {options.map((item) => (
-          <option value={item.value} key={item.label}>
-            {item.label}
-          </option>
-        ))}
-      </S.SelectInput>
-    </S.SelectContent>
+    <Select.Root>
+      <S.SelectTriggerContent aria-label="Categories">
+        <Select.Value placeholder={placeholder} />
+
+        <S.SelectIcon>
+          <CaretDown weight="bold" />
+        </S.SelectIcon>
+      </S.SelectTriggerContent>
+
+      <Select.Portal>
+        <S.SelectContent>
+          <S.SelectScrollUpButton>
+            <CaretUp weight="bold" />
+          </S.SelectScrollUpButton>
+
+          <S.SelectViewport>
+            <S.SelectGroup>
+              {!!selectLabel && <S.SelectLabel>Categorias</S.SelectLabel>}
+
+              {options.map((item) => (
+                <S.SelectItem value={item.value} key={item.value}>
+                  <Select.ItemText>{item.label}</Select.ItemText>
+
+                  <Select.ItemIndicator>
+                    <Check weight="bold" />
+                  </Select.ItemIndicator>
+                </S.SelectItem>
+              ))}
+            </S.SelectGroup>
+          </S.SelectViewport>
+
+          <Select.ScrollDownButton>
+            <CaretDown weight="bold" />
+          </Select.ScrollDownButton>
+        </S.SelectContent>
+      </Select.Portal>
+    </Select.Root>
   );
 };
 
-export default Select;
+export default SelectInput;
--- src/components/Select/styles.ts
+++ src/components/Select/styles.ts
@@ -1,55 +1,117 @@
 import styled, { css } from "styled-components";
+import * as Select from "@radix-ui/react-select";
 
-import { ISelectContentProps } from ".";
-
-const variant = {
-  default: () => css`
-    border-radius: 0.25rem;
-  `,
-  full: () => css`
-    border-radius: 1.5625rem;
-  `,
-  withBoxShadow: () => css`
-    box-shadow: 0px 4px 10px 0px rgba(0, 0, 0, 0.04);
-  `
-};
-
-export const SelectContent = styled.div<ISelectContentProps>`
-  ${({ theme, borderRadius, hasBoxShadow }) => css`
-    width: 31.25rem;
+export const SelectTriggerContent = styled(Select.SelectTrigger)`
+  ${({ theme }) => css`
+    width: 100%;
     height: 3.125rem;
     padding: 0.5rem 1rem;
     background: ${theme.colors.white};
     border: 2px solid transparent;
+    border-radius: 0.25rem;
+
+    box-shadow: 0px 4px 10px 0px rgba(0, 0, 0, 0.04);
 
     flex-shrink: 0;
 
-    &:focus-within {
-      border-color: ${theme.colors.primary.main};
+    display: inline-flex;
+    align-items: center;
+    justify-content: space-between;
+
+    &:focus {
+      box-shadow: 0 0 0 2px ${theme.colors.primary.main};
     }
 
-    ${!!borderRadius && variant[borderRadius]}
-    ${!!hasBoxShadow && variant.withBoxShadow}
-  `}
-`;
-
-export const SelectInput = styled.select`
-  ${({ theme }) => css`
-    font-family: ${theme.font.family};
-    background: transparent;
-    border: 0;
-    outline: none;
-    width: 100%;
-    height: 100%;
-
-    &::placeholder {
+    &[data-placeholder] {
       color: ${theme.colors.gray[200]};
     }
+
+    & button {
+      margin-top: 0;
+    }
   `}
 `;
 
-export const SelectPlaceholder = styled.option`
+export const SelectIcon = styled(Select.Icon)`
   ${({ theme }) => css`
+    color: ${theme.colors.gray[900]};
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `}
+`;
+
+export const SelectContent = styled(Select.Content)`
+  ${({ theme }) => css`
+    overflow: hidden;
+    background-color: ${theme.colors.white};
+    border-radius: 0.25rem;
+    box-shadow: 0px 10px 38px -10px rgba(22, 23, 24, 0.35),
+      0px 10px 20px -15px rgba(22, 23, 24, 0.2);
+  `}
+`;
+
+export const SelectScrollUpButton = styled(Select.ScrollUpButton)`
+  ${({ theme }) => css`
+    height: 1.5625rem;
+    background-color: ${theme.colors.white};
+    color: ${theme.colors.gray[900]};
+    cursor: default;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `}
+`;
+
+export const SelectViewport = styled(Select.Viewport)`
+  padding: 5px;
+`;
+
+export const SelectLabel = styled(Select.Label)`
+  ${({ theme }) => css`
+    padding: 0 1.5625rem;
+    font-size: 12px;
+    line-height: 1.5625rem;
     color: ${theme.colors.gray[200]};
   `}
+`;
+
+export const SelectGroup = styled(Select.Group)``;
+
+export const SelectItem = styled(Select.Item)`
+  ${({ theme }) => css`
+    font-size: 13px;
+    line-height: 1;
+    border-radius: 0.25rem;
+    height: 1.5625rem;
+    padding: 0 2.1875rem 0 1.5625rem;
+    position: relative;
+    user-select: none;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    &[data-disabled] {
+      color: ${theme.colors.gray[100]};
+      pointer-events: none;
+    }
+    &[data-highlighted] {
+      outline: none;
+      background-color: ${theme.colors.primary.lighter};
+      color: ${theme.colors.primary.dark};
+    }
+  `}
+`;
+
+export const SelectItemIndicator = styled(Select.ItemIndicator)`
+  position: absolute;
+  left: 0;
+  width: 1.5625rem;
+
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
 `;
