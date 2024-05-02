25625f57e1da0084e93950a69fb129b188fbfff8
Daniel Silva <dfsilva.dxp@gmail.com>
2024-05-02 18:32:20 -0300

    feat: add breadcrumb, flex, form, search e select.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,13 @@
 export { default as Badge } from "@components/Badge";
+export { default as Breadcrumb } from "@components/Breadcrumb";
 export { default as Button } from "@components/Button";
 export { default as ContactCard } from "@components/ContactCard";
 export { default as ContactsHeader } from "@components/ContactsHeader";
 export { default as ContactsList } from "@components/ContactsList";
 export { default as Container } from "@components/Container";
+export { default as Flex } from "@components/Flex";
+export { default as Form } from "@components/Form";
 export { default as Header } from "@components/Header";
 export { default as Input } from "@components/Input";
-export { default as Search } from "@components/Header";
+export { default as Search } from "@components/Search";
+export { default as Select } from "@components/Select";
