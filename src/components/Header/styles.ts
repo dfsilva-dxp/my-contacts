c6631cb62ac52cd74879f75b1b9b26a641ad82ae
Daniel Silva <dfsilva.dxp@gmail.com>
2024-05-02 18:25:41 -0300

    feat: add header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,9 +1,6 @@
 import styled from "styled-components";
 
 export const HeaderContent = styled.header`
-  display: flex;
-  justify-content: center;
-  align-items: center;
   padding-top: 4.125rem;
   padding-bottom: 1rem;
 `;
