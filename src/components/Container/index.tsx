b501c76d50184f8ad535a4eb73683f1fa0d2fcf2
Daniel Silva <dfsilva.dxp@gmail.com>
2024-05-24 18:16:42 -0300

    nit: edit main.

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,6 +1,6 @@
 import styled, { css } from "styled-components";
 
-const Container = styled.main`
+const Container = styled.div`
   ${({ theme }) => css`
     width: 100%;
     max-width: ${theme.grid.container.default};
