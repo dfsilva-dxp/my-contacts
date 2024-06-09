b4b95844031589921700c6134fc52045591f408c
Daniel Silva <dfsilva.dxp@gmail.com>
2024-06-09 10:41:46 -0300

    nit: delete file.

--- src/components/ContactsList/types.d.ts
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-import { IContactsHeaderProps } from "../ContactsHeader/types";
-
-interface IContactsListProps extends IContactsHeaderProps {
-  children: ReactNode;
-}
-
-export { IContactsListProps };
