8d4f1782d251f2a1060fd248b666df96ca61f56f
Daniel Silva <dfsilva.dxp@gmail.com>
2024-06-17 19:00:11 -0300

    chore: add react-toastify.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,23 @@
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
+import { ToastContainer } from "react-toastify";
 
 import { DefaultRoutes } from "@/routes";
 
 import { DefaultTheme, GlobalStyles } from "@/styles";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={DefaultTheme}>
         <GlobalStyles />
+        <ToastContainer
+          autoClose={3000}
+          theme="colored"
+          icon={false}
+          position="bottom-right"
+        />
         <DefaultRoutes />
       </ThemeProvider>
     </BrowserRouter>
